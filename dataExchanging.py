065e5cd92cee57167b73d60ed8d15557a51aa050
AydenRosinkranz <leonhartdreyse@gmail.com>
2023-02-13 18:03:32 +0800

    good! zip-reading-fixed

--- dataExchanging.py
+++ dataExchanging.py
@@ -1,8 +1,8 @@
-import pandas as pd
 import os
-import zipfile
-os.chdir('E:\Strategy\MachineLearning')
-with zipfile.ZipFile('data_formyFirstGithubRepo.zip','r') as zip_ref:
-    for filename in zip_ref.namelist():
-        if filename.endswith('.csv'):
-            df = pd.read_csv(zip_ref.open(filename))
+import pandas as pd
+from zipfile import ZipFile
+os.chdir('E:/Strategy/MachineLearning')
+zip_file = ZipFile('data_formyFirstGithubRepo.zip')
+dfs = {text_file.filename: pd.read_csv(zip_file.open(text_file.filename),encoding='unicode_escape',on_bad_lines='skip')
+       for text_file in zip_file.infolist()
+       if text_file.filename.endswith('.csv')}
